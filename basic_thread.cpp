26ee6e6a864e6943164e7173b3885cd3a648360c
ryanpig <ryanpig@gmail.com>
2019-02-19 17:21:03 +0100

    add different ways to create thread in the basic version

--- basic_thread.cpp
+++ basic_thread.cpp
@@ -1,3 +1,6 @@
+/* Goal: familar w/ different ways to create thread
+ * Expect outcome: the issue comes from the race of shared resource (ofstream)
+ */
 #include <thread>
 #include <iostream>
 #include <chrono>
@@ -8,22 +11,48 @@ void func(int n)
 {
   for(int i = 0; i < n ; i++){
     cout << "(child)thread id:" << this_thread::get_id() << ", value:" << i << endl;
-    this_thread::sleep_for(chrono::milliseconds(100));
-    
+    // this_thread::sleep_for(chrono::milliseconds(100));
   }
-
 }
+
+class Obj
+{
+  public:
+    void operator()(int n)
+    {
+      for(int i = 0; i < n ; i++){
+        cout << "(obj func)thread id:" << this_thread::get_id() << ", value:" << i << endl;
+        // this_thread::sleep_for(chrono::milliseconds(100));
+      }
+    }
+};
 
 int main()
 {
+  // call thread constructor w/ function name
+  thread t1(func, 20);
 
-  thread t(func, 20);
 
+  // call thread constructor w/ lambda function 
+  thread t2([](int n){
+    cout << "(lambda)thread id:" << this_thread::get_id() << ", value:" << n << endl;
+    // this_thread::sleep_for(chrono::milliseconds(100));
+    }, 20);
+
+  // call thread constructor w/ member function of object 
+  thread t3(Obj(), 20); 
+  
+  // from main thread
   for(int i = 0; i < 10 ; i++){
     cout << "(parent)thread id:" << this_thread::get_id() << ", value:" << i << endl;
-    this_thread::sleep_for(chrono::milliseconds(70));
-
+    // this_thread::sleep_for(chrono::milliseconds(70));
   }
 
-  t.join();
+  // block
+  if(t1.joinable())
+    t1.join();
+  if(t2.joinable())
+    t2.join();
+  if(t3.joinable())
+    t3.join();
 }
